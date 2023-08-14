021fabd68594a54667e8d0ce425a4d0288495c81
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-08-14 19:41:04 -0300

    main for styles and loading for usage

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import './form'
+
 import '../styles/global.css'
 import '../styles/base.css'
 import '../styles/form.css'
+import '../styles/transcription.css'
+import '../styles/copy.css'
+import '../styles/loading.css'
